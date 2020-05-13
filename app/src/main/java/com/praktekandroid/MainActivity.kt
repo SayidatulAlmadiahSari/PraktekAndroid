b5eaaaa4b1a826f41c3c7a68218464d851d34cc8
SayidatulAlmadiahSari <sayidatul.almadiah@gmail.com>
2020-05-13 12:02:38 +0800

    Game ON

--- app/src/main/java/com/praktekandroid/MainActivity.kt
+++ app/src/main/java/com/praktekandroid/MainActivity.kt
@@ -8,22 +8,27 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-      val textView1 = findViewById<TextView>(R.id.textView1)
-      val button1 = findViewById<Button>(R.id.button1)
+        val textView1 = findViewById<TextView>(R.id.textView1)
+        val button1 = findViewById<Button>(R.id.button1)
 
 
         textView1.setOnClickListener {
-            Toast.makeText(this,"hai",Toast.LENGTH_SHORT ).show()
+            Toast.makeText(this, "hai", Toast.LENGTH_SHORT).show()
         }
 
         button1.setOnClickListener {
             val intent = Intent(this@MainActivity, HitungActivity::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
             startActivity(intent)
         }
     }
